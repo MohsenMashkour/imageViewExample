e4883d3dcd1557c4e451d7edebf0a5b5d94a647f
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2022-05-04 13:19:59 +0200

    Update MainActivity.kt

--- app/src/main/java/com/mkrdeveloper/imageviewexample/MainActivity.kt
+++ app/src/main/java/com/mkrdeveloper/imageviewexample/MainActivity.kt
@@ -11,22 +11,31 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val img = findViewById<ImageView>(R.id.imageView)
+         img = findViewById<ImageView>(R.id.imageView)
         val btn = findViewById<TextView>(R.id.button)
 
-        val bitmap = BitmapFactory.decodeResource(this.resources,R.drawable.dal_logo)
-         val uri = Uri.parse("android.resource://com.mkrdeveloper.imageviewexample/drawable/dal_logo")
+
 
         btn.setOnClickListener {
+        setImageFunctions()
 
-           // img.setImageResource(R.drawable.dal_logo)
-             //img.setImageBitmap(bitmap)
-           //  img.setImageDrawable(ContextCompat.getDrawable(applicationContext,R.drawable.dal_logo))
-             img.setImageURI(uri)
         }
+    }
+
+    fun setImageFunctions(){
+        val bitmap = BitmapFactory.decodeResource(this.resources,R.drawable.dal_logo)
+        val uri = Uri.parse("android.resource://com.mkrdeveloper.imageviewexample/drawable/dal_logo")
+
+        // img.setImageResource(R.drawable.dal_logo)
+        //img.setImageBitmap(bitmap)
+        //  img.setImageDrawable(ContextCompat.getDrawable(applicationContext,R.drawable.dal_logo))
+        img.setImageURI(uri)
     }
 }
